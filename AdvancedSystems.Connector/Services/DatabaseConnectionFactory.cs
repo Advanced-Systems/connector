0524efb849e05bc27ec2e2380fb99b6b3406f1f0
StefanGreve <greve.stefan@outlook.jp>
2024-08-08 20:34:48 +0200

    Refactor factory code: remove unused logger

--- AdvancedSystems.Connector/Services/DatabaseConnectionFactory.cs
+++ AdvancedSystems.Connector/Services/DatabaseConnectionFactory.cs
@@ -3,18 +3,15 @@
 using AdvancedSystems.Connector.Abstractions;
 
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace AdvancedSystems.Connector.Services;
 
 public sealed class DatabaseConnectionFactory : IDatabaseConnectionFactory
 {
-    private readonly ILogger<DatabaseConnectionFactory> _logger;
     private readonly IServiceProvider _serviceProvider;
 
-    public DatabaseConnectionFactory(ILogger<DatabaseConnectionFactory> logger, IServiceProvider serviceProvider)
+    public DatabaseConnectionFactory(IServiceProvider serviceProvider)
     {
-        this._logger = logger;
         this._serviceProvider = serviceProvider;
     }
 
@@ -23,7 +20,7 @@ public sealed class DatabaseConnectionFactory : IDatabaseConnectionFactory
         return provider switch
         {
             Provider.MsSql => this._serviceProvider.GetRequiredService<MsSqlServerConnectionService>(),
-            _ => throw new NotImplementedException(),
+            _ => throw new NotImplementedException(Enum.GetName(provider)),
         };
     }
 }
